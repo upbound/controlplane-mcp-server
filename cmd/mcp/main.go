3f9abc16e90edea7c9d6244926ca769f4f87ebf4
tnthornton <2375126+tnthornton@users.noreply.github.com>
2025-07-02 20:35:55 -0700

    setup up golangci-lint

--- cmd/mcp/main.go
+++ cmd/mcp/main.go
@@ -1,7 +1,10 @@
+/*
+Package main is the main entrypoint into the mcp server.
+*/
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("doesn't do anything yet")
+	fmt.Println("doesn't do anything yet") //nolint:forbidigo //setting up repo
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/upbound/controlplane-mcp-server
 
-go 1.23.10
+go 1.24.0
