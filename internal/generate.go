3494a2e515d5a94c5cd0e34124dad3b1de854b95
tnthornton <2375126+tnthornton@users.noreply.github.com>
2025-08-05 14:47:46 -0700

    Add license headers and tooling

--- internal/generate.go
+++ internal/generate.go
@@ -14,17 +14,8 @@
 // limitations under the License.
 // */
 
-//go:build !custombootcheck
-// +build !custombootcheck
+// Add license headers to all files.
+//go:generate go tool addlicense -c "Upbound Inc" -v -f ../hack/boilerplate.go.txt . ../cmd
 
-/*
-Package bootcheck provides helpers for checks at boot time.
-*/
-package bootcheck
-
-// CheckEnv is a no-op by default. Use build tags for build-time isolation of
-// custom preflight checks. Ensure to update the build tags on L1-L2 so that
-// they are mutually exclusive across implementations.
-func CheckEnv() error {
-	return nil
-}
+// Package internal provides internal packages for this repo.
+package internal
